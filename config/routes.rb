df4fe3f00b0d5bee059d92776118164bd4538159
anuskapant <anuska.pant@gmail.com>
2020-10-16 11:14:18 +0545

    recipe_new  added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'recipes/show'
-  get 'recipes/index'
-  get 'recipes/edit'
-  get 'recipes/show'
-  get 'recipes/index'
-  get 'recipe/index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
+  # resources :recipes
   resources :recipes
+    
 end
